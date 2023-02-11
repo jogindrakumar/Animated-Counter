445ac2fc25517ccf728bc7be64cc061b269ce180
jogindrakumar <joginder9868@outlook.com>
2023-02-11 14:27:26 +0530

    animated-counter

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //Codewithjk
+
+const counters = document.querySelectorAll(".counter");
+const speed = 200; // lower the slower
+
+counters.forEach((counter) => {
+    const updateCount = () => {
+        const target = +counter.getAttribute("data-target");
+        const count = +counter.innerText;
+
+        //lower inc to slow and higher to slow
+
+        const inc = target / speed;
+
+        if (count < target) {
+            counter.innerText = count + inc;
+
+            //call function every ms
+            setTimeout(updateCount, 1);
+        } else {
+            counter.innerText = target;
+        }
+    };
+    updateCount();
+});
